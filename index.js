efde8e36ca812255c5d051810f6f3887c93cdf14
Scott Moschella <scottmosch@gmail.com>
2018-03-18 12:41:53 -0700

    Listen on 3000 or on env.PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = require('./server/server-config');
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(port);
 
