2df97dfca47d69196e812c060f77c8abfecb4089
Scott Moschella <scottmosch@gmail.com>
2018-03-19 19:57:57 -0700

    Add bodyParser and a post route

--- server/server-config.js
+++ server/server-config.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const db = require('../db-config');
+const bodyParser = require('body-parser')
+
 const userController = require('./controllers/users');
+const songController = require('./controllers/songs');
 
 const app = express();
 app.use(express.static('build'));
+app.use(bodyParser.json());
 
 app.get('/hello', userController.hello);
+
+app.post('/api/song', songController.newSong);
 
 module.exports = app;
