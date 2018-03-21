003affe67108a44c30eb13ff79fcfe6a5793e6bc
Scott Moschella <scottmosch@gmail.com>
2018-03-21 14:17:09 -0700

    Add react route to handle song edit page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,6 +8,7 @@ const App = () => {
     <BrowserRouter>
       <Switch>
         <Route path='/song/new' render={(props) => <SongInputForm {...props} />} />
+        <Route path='/song/:id/edit' render={(props) => <SongInputForm editing={true} {...props} />} />
         <Route path='/songs' render={(props) => <Songs {...props} />} />
       </Switch>
     </BrowserRouter>
