ab489dd7c27ccb3270a1c714c71e08b7fe9ed41e
Scott Moschella <scottmosch@gmail.com>
2018-03-21 00:04:11 -0700

    Add an api route for viewing all songs

--- server/server-config.js
+++ server/server-config.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('../db-config');
 const bodyParser = require('body-parser')
+const path = require('path');
 
 const userController = require('./controllers/users');
 const songController = require('./controllers/songs');
@@ -10,5 +11,11 @@ app.use(express.static('build'));
 app.use(bodyParser.json());
 
 app.post('/api/song', songController.newSong);
+
+app.get('/api/songs', songController.getAllSongs);
+
+app.get('/*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../build/index.html'));
+});
 
 module.exports = app;
