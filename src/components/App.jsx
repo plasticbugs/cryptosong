491f6c22af5edece6905a4b493b95a8b105f2242
Scott Moschella <scottmosch@gmail.com>
2018-03-21 00:04:35 -0700

    Wrap app with React router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import SongInputForm from './SongInputForm.jsx';
+import Songs from './Songs.jsx';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 // import ScatterChart from './ScatterChart.jsx';
-const App = () =>
-  (
-    <SongInputForm />
-  );
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/song/new' render={(props) => <SongInputForm {...props} />} />
+        <Route path='/songs' render={(props) => <Songs {...props} />} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
 
 module.exports = App;
