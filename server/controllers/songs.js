120d1e8e7a7dda3f35e46ae1d3247740ba02b1fd
Scott Moschella <scottmosch@gmail.com>
2018-03-21 00:03:15 -0700

    Add a handler to return all songs from the DB

--- server/controllers/songs.js
+++ server/controllers/songs.js
@@ -1,3 +1,5 @@
+const Song = require('../models/song.js');
+
 module.exports.newSong = (req, res) => {
   console.log(req.body)
   res.send('Success')
@@ -5,4 +7,11 @@ module.exports.newSong = (req, res) => {
 
 module.exports.editSong = (req, res) => {
 
+}
+
+module.exports.getAllSongs = (req, res) => {
+  Song.find().then( results => {
+    console.log(results);
+    res.send(results);
+  })
 }
