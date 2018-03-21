afa7889299766a6cc23d5d57adfc642ffa809fd6
Scott Moschella <scottmosch@gmail.com>
2018-03-21 14:40:51 -0700

    Remove 'randomcolor' library and references

--- src/components/Songs.jsx
+++ src/components/Songs.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Card, Icon, Image } from 'semantic-ui-react';
 import axios from 'axios';
-var randomColor = require('randomcolor'); // import the script
-// var color = randomColor(); // a hex code for an attractive color
 
 export default class Songs extends Component {
   constructor(props) {
@@ -21,14 +19,12 @@ export default class Songs extends Component {
   }
 
 
-  // <a href="https://placeholder.com"><img src="http://via.placeholder.com/350x150"></a>
   render() {
     return (<Container>
       <Card.Group itemsPerRow={4}>
     {this.state.songs.map( (song) => {
       return (
         <Card key={song.number}>
-          {/* <Image src={`http://via.placeholder.com/800x800/${randomColor({hue: 'blue'}).slice(1)}/ffffff`} /> */}
           <Image src={`https://img.youtube.com/vi/${song.videoid}/mqdefault.jpg`} />
           <Card.Content>
             <Card.Header>
