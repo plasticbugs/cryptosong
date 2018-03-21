0ecddbe2616bfbffed15342b4111670ab0548b83
Scott Moschella <scottmosch@gmail.com>
2018-03-21 00:03:36 -0700

    Configure mongodb and mongoose

--- db-config.js
+++ db-config.js
@@ -1,25 +1,34 @@
-const Sequelize = require('sequelize');
-const path = require('path');
+// const Sequelize = require('sequelize');
+// const path = require('path');
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'sqlite',
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-  storage: path.join(__dirname, '/db/database.sqlite'),
+// const sequelize = new Sequelize('database', 'username', 'password', {
+//   host: 'localhost',
+//   dialect: 'sqlite',
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+//   storage: path.join(__dirname, '/db/database.sqlite'),
+// });
+
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log('Connection has been established successfully.');
+//   })
+//   .catch((err) => {
+//     console.error('Unable to connect to the database:', err);
+//   });
+
+// module.exports = sequelize;
+
+
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost/cryptosong', {
+  promiseLibrary: global.Promise
 });
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
-
-module.exports = sequelize;
+module.exports = mongoose.connection;
