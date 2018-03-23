622f3861d954da12745085073a40602c5eb01334
Scott Moschella <scottmosch@gmail.com>
2018-03-22 21:58:35 -0700

    Refactor songSchema to store id references to other collections

--- server/models/song.js
+++ server/models/song.js
@@ -1,17 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// const Instrument = mongoose.model('Instrument', {name: String, image: String});
+const Inkey = mongoose.model('Inkey', {name: String, image: String});
+const Topic = mongoose.model('Topic', {name: String, image: String});
+const Beard = mongoose.model('Beard', {name: String, image: String});
+const City = mongoose.model('Location', {name: String, image: String});
+
 const songSchema = new Schema({
   number: Number,
   title: String,
   date: Date,
   length: Number,
-  inkey: String,
+  inkey: { type: Schema.Types.ObjectId, ref: 'Inkey' },
   tempo: Number,
-  topic: String,
-  location: String,
+  topic: { type: Schema.Types.ObjectId, ref: 'Topic' },
+  location: { type: Schema.Types.ObjectId, ref: 'Location' },
   instruments: [{ type: Schema.Types.ObjectId, ref: 'Instrument' }],
-  beard: String,
+  beard: { type: Schema.Types.ObjectId, ref: 'Beard' },
   videoid: String,
   description: String,
   acousticproduced: String,
