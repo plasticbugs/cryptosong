e8adcd9e4ee4e6231c6c3a6d980e1967b6822d13
Scott Moschella <scottmosch@gmail.com>
2018-03-23 13:18:10 -0700

    Pink!

--- src/components/AlbumCanvas.jsx
+++ src/components/AlbumCanvas.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Stage, Image, Layer } from "react-konva";
+import { Stage, Image, Layer, Rect } from "react-konva";
 import SongImage from './SongImage.jsx';
 
 export default class AlbumCanvas extends Component {
@@ -86,8 +86,16 @@ export default class AlbumCanvas extends Component {
 
   render() {
     return (
-      <Stage width={500} height={500}>
+      <Stage width={500} height={500} >
         <Layer>
+          <Rect
+            x={0}
+            y={0}
+            width={500}
+            height={500}
+            fill="pink"
+            shadowBlur={2}
+          />
           {this.props.images.map(image => {
             return <SongImage imageSrc={`/images/${image}`} />
           })}
