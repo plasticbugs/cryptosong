c22157bad708179e9c70040c3705179866716e3a
Scott Moschella <scottmosch@gmail.com>
2018-03-25 22:14:26 -0700

    Refactor and clean up placeholder code

--- src/components/SongInputForm.jsx
+++ src/components/SongInputForm.jsx
@@ -7,115 +7,44 @@ import moment from 'moment';
 
 import AlbumCanvas from './AlbumCanvas.jsx';
 
-const keyOptions = [
-  {key: "A", text: "A", value: "A"},
-  {key: "Am", text: "Am", value: "Am"},
-  {key: "B", text: "B", value: "B"},
-  {key: "Bb", text: "Bb", value: "Bb"},
-  {key: "Bbm", text: "Bbm", value: "Bbm"},
-  {key: "Bm", text: "Bm", value: "Bm"},
-  {key: "C", text: "C", value: "C"},
-  {key: "D", text: "D", value: "D"},
-  {key: "D#m", text: "D#m", value: "D#m"},
-  {key: "Em", text: "Em", value: "Em"},
-  {key: "F", text: "F", value: "F"},
-  {key: "F#", text: "F#", value: "F#"},
-  {key: "G", text: "G", value: "G"},
-  {key: "Gm", text: "Gm", value: "Gm"},
-]
-
-const topicOptions = [
-  {key: "Airport", text: "Airport", value: "Airport"},
-  {key: "Animals", text: "Animals", value: "Animals"},
-  {key: "Anxiety", text: "Anxiety", value: "Anxiety"},
-  {key: "Apple", text: "Apple", value: "Apple"},
-  {key: "Commission", text: "Commission", value: "Commission"},
-  {key: "Friend", text: "Friend", value: "Friend"},
-  {key: "Internet", text: "Internet", value: "Internet"},
-  {key: "Life", text: "Life", value: "Life"},
-  {key: "Love", text: "Love", value: "Love"},
-  {key: "Poetic", text: "Poetic", value: "Poetic"},
-  {key: "Politics", text: "Politics", value: "Politics"},
-  {key: "Sex", text: "Sex", value: "Sex"},
-  {key: "Sick", text: "Sick", value: "Sick"},
-  {key: "Social Justice", text: "Social Justice", value: "Social Justice"},
-  {key: "Song A Day", text: "Song A Day", value: "Song A Day"},
-  {key: "Video Games", text: "Video Games", value: "Video Games"},
-]
-
-const instrumentOptions = [
-  {key:"vocals","text":"Vocals","value":"vocals"},
-  {key:"baritone uke","text":"Baritone Uke","value":"baritone uke"},
-  {key:"synths","text":"Synths","value":"synths"},
-  {key:"drum machine","text":"Drum Machine","value":"drum machine"},
-  {key:"snaps","text":"Snaps","value":"snaps"},
-  {key:"drums","text":"Drums","value":"drums"},
-  {key:"organ","text":"Organ","value":"organ"},
-  {key:"electric guitar","text":"Electric Guitar","value":"electric guitar"}
-]
-
-const beardOptions = [
-  {key: "Stubble", text: "Stubble", value: "Stubble"},
-  {key: "N/A", text: "N/A", value: "N/A"},
-  {key: "Clean", text: "Clean", value: "Clean"},
-  {key: "Shadow", text: "Shadow", value: "Shadow"},
-]
-
-
-const buildOptions = (arr) => {
-  let obj = {};
-  arr.forEach(key => {
-    if (!obj[key]) {
-      obj[key] = true;
-    }
-  })
-  let solution = [];
-  Object.keys(obj).forEach((key, index) => {
-    solution.push({
-      key,
-      text: key,
-      value: key
-    })
-  })
-  return solution;
-}
 
 export default class SongInputForm extends Component {
-  // constructor (props) {
-    // super(props)
-    // this.state = {
-    //   date: moment()
-    // };
-    // this.handleChange = this.handleChange.bind(this);
-    // this.handleRadioChange = this.handleRadioChange.bind(this);
-  // }
 
   constructor(props) {
     super(props);
     this.state = {
+      inkeyOptions: [{key:"",text:"",value:""}],
+      beardOptions: [{key:"",text:"",value:""}],
+      instrumentOptions: [{key:"",text:"",value:""}],
+      topicOptions: [{key:"",text:"",value:""}],
+      locationOptions: [{key:"",text:"",value:""}],
+      instrument: [],
+      inkey: [],
+      topic: [],
+      location: [],
+      beard: [],
       song: {
         number: '',
         title: '',
         date: moment(),
         mins: '',
         secs: '',
-        inkey: '',
+        inkey: {name: ''},
         tempo: '',
-        topic: '',
-        beard: '',
+        topic: {name: ''},
+        beard: {name: ''},
         videoid: '',
         description: '',
         acousticproduced: '',
         instruments: [],
-        images: []
       },
-      images: []
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleDateChange = this.handleDateChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleDropdownChange = this.handleDropdownChange.bind(this);
     this.getInstrumentNames = this.getInstrumentNames.bind(this);
+    this.setUpDropdowns = this.setUpDropdowns.bind(this);
     // this.setUpCanvas = this.setUpCanvas.bind(this);
   }
 
@@ -124,23 +53,14 @@ export default class SongInputForm extends Component {
     song.date = moment(song.date);
     song.secs = song.length % 60
     song.mins = Math.floor(song.length / 60);
-    song.images = [];
     song.instruments = song.instruments.map(instrument => {
       // song.images.push(instrument.image)
       return instrument.name;
     })
-    song.images.push(song.beard.image)
-    // song.images.push(song.location.image)
-    song.images.push(song.inkey.image)
-    // song.images.push(song.topic.image)
-    song.beard = song.beard.name;
-    song.location = song.location.name;
-    song.topic = song.topic.name;
-    song.inkey = song.inkey.name;
     return song;
   }
 
-  componentDidMount() {
+  componentWillMount() {
     if(this.props.match.params.id) {
       axios.get('/api/song', {
         params:
@@ -149,27 +69,41 @@ export default class SongInputForm extends Component {
           }
         })
       .then(response => {
-        console.log(response.data[0])
-        let song = this.formatSong(response.data[0]);
+        console.log(response.data)
+        let song = this.formatSong(response.data.song);
         this.handleDateChange(song.date);
-        // this.setState({selected: ['vocals']})
-        this.setState({song}, () => {
-          console.log(song);
-        })
-        // this.setState({song: {instruments: song.instruments}}, ()=> {
-        //   delete song.instruments
-        // })
-        // for (let key in song) {
-        //   this.handleChange(null, {
-        //     name: key,
-        //     value: song[key]
-        //   })
-        // }
+        this.setUpDropdowns(response.data, song);
+
       })
     }
-    // if (this.props.match)
   }
 
+  setUpDropdowns({beard, instrument, inkey, location, topic}, song) {
+    let instrumentOptions = [];
+    let beardOptions = [];
+    let inkeyOptions = [];
+    let locationOptions = [];
+    let topicOptions = [];
+
+    
+
+    let tagArray = [{beard}, {instrument}, {inkey}, {location}, {topic}]
+    let options = {};
+    tagArray.forEach(tag => {
+      let optionArray = [];
+      let key = Object.keys(tag)[0];
+      tag[key].forEach(element => {
+        optionArray.push({key: element.name, text: element.name, value: element.name});
+      })
+      let theKey = key + 'Options';
+      options[theKey] = optionArray;
+    })
+    console.log(options)
+
+    this.setState({beard, instrument, inkey, location, topic, ...options}, () => {
+      this.setState({song});
+    });
+  }
 
   handleDateChange(date) {
     let song = Object.assign({}, this.state.song, {date})
@@ -179,32 +113,41 @@ export default class SongInputForm extends Component {
   }
 
   handleChange(e, { name, value }) {
-    // if (fullSong) {
-    //   let song = Object.assign({}, fullSong);
-    console.log(name, value)
-    // } else {
-      let song = Object.assign({}, this.state.song, {[name]: value});
-      this.setState({song})
-    // }
+
+    let arr = this.state[name];
+
+    if (!Array.isArray(arr)) {
+      this.setState(prevState => ({
+        ...prevState,
+        song: {
+          ...prevState.song,
+          [name]: value
+        }
+      }))
+    } else {
+      let selectedItem = arr.find(element => {
+        return element.name === value;
+      })
+      this.setState(prevState => ({
+        ...prevState,
+        song: {
+          ...prevState.song,
+          [name]: selectedItem
+        }
+      }))
+    }
   }
 
   handleDropdownChange(e, { name, value }, fullSong) {
-    // if (fullSong) {
-    //   let song = Object.assign({}, fullSong);
-
-    // } else {
       let song = Object.assign({}, this.state.song, {[name]: value});
       this.setState({song})
-    // }
   }
 
 
   handleSubmit() {
     axios.post('/api/song', this.state.song)
     .then(response => {
-      console.log(response);
     })
-    // console.log(this.state)
   }
 
   renderHeader() {
@@ -242,7 +185,7 @@ export default class SongInputForm extends Component {
       acousticproduced,
       instruments
     } = this.state.song
-
+    // console.log(beard)
     return (
       <Container>
         <Grid columns={2} divided>
@@ -274,13 +217,13 @@ export default class SongInputForm extends Component {
                       <Input name='secs' placeholder='SS' value={secs} onChange={this.handleChange} />
                     </Form.Field>
                   </Form.Group>
-                  <Form.Field control={Select} label='Song Key' value={inkey} name='inkey' options={keyOptions} placeholder='Choose Key' onChange={this.handleChange} />
+                  <Form.Field control={Select} label='Song Key' value={inkey.name} name='inkey' options={this.state.inkeyOptions} placeholder='Choose Key' onChange={this.handleChange} />
                   <Form.Field>
                     <label>Tempo</label>
                     <Input name='tempo' value={tempo} type='number' width={4} onChange={this.handleChange} />
                   </Form.Field>
-                  <Form.Field control={Select} value={topic} name='topic' label='Topic' options={topicOptions} placeholder='Choose a Topic' onChange={this.handleChange} />
-                  <Form.Field control={Select} value={beard} name='beard' label='Beard' options={beardOptions} placeholder='Choose a Beard' onChange={this.handleChange} />
+                  <Form.Field control={Select} value={topic.name} name='topic' label='Topic' options={this.state.topicOptions} placeholder='Choose a Topic' onChange={this.handleChange} />
+                  <Form.Field control={Select} value={beard.name} name='beard' label='Beard' options={this.state.beardOptions} placeholder='Choose a Beard' onChange={this.handleChange} />
                   <Form.Field>
                     <label>YT Video ID or Link</label>
                     <Input name='videoid' value={videoid} placeholder='YT Video ID' onChange={this.handleChange} />
@@ -313,10 +256,10 @@ export default class SongInputForm extends Component {
                       selection
                       multiple
                       value={instruments}
-                      options={instrumentOptions}
+                      options={this.state.instrumentOptions}
                       allowAdditions
                       onAddItem={(e, d)=> {
-                        instrumentOptions.push({key: d.value, text: d.value, value: d.value})
+                        this.state.instrumentOptions.push({key: d.value, text: d.value, value: d.value})
                       }}
                       onChange={this.handleDropdownChange}
                     />
@@ -325,7 +268,7 @@ export default class SongInputForm extends Component {
                 </Form>
             </Grid.Column>
             <Grid.Column>
-              <AlbumCanvas images={this.state.song.images}/>
+              {/* <AlbumCanvas images={this.state.song.images}/> */}
             </Grid.Column>
           </Grid.Row>
         </Grid>
