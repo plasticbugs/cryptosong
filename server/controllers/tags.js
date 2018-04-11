c5bcf8d059aa12368cf347e7d90c2914a396127b
Scott Moschella <scottmosch@gmail.com>
2018-04-10 22:21:37 -0700

    Add controller for tagging functionality

---
+++ server/controllers/tags.js
@@ -0,0 +1,45 @@
+const TagModel = require('../models/tag');
+
+module.exports.saveNewTag = (req, res) => {
+  let tag = req.body.tag;
+  let songID = Number.parseInt(req.body.songID);
+  TagModel.insertTag(tag, songID)
+  .then(tag => {
+    res.send(tag)
+  })
+}
+
+module.exports.getTags = (req, res) => {
+  TagModel.getAll()
+  .then(results => {
+    res.send(results);
+  })
+}
+
+module.exports.updateTags = (req, res) => {
+  let tags = req.body;
+  TagModel.updateAll(tags)
+  .then(results => {
+    res.send(results)
+  })
+  .catch(err => {
+    if (err) {
+      console.log(err)
+      res.sendStatus(501);
+    }
+  })
+}
+
+module.exports.deleteTags = (req, res) => {
+  let tags = req.query.delete;
+  TagModel.deleteMany(tags)
+  .then(results => {
+    res.sendStatus(200);
+  })
+  .catch(err => {
+    if (err) {
+      console.log(err);
+      res.send(err)
+    }
+  })
+}
