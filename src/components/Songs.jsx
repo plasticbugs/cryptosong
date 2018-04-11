cc592fe03ca3aeccd3f9f8ac81e6cd67a1e2eebd
Scott Moschella <scottmosch@gmail.com>
2018-04-10 22:26:20 -0700

    Temporarily reconfigure songs

--- src/components/Songs.jsx
+++ src/components/Songs.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Card, Icon, Image } from 'semantic-ui-react';
+import { Container, Card, Icon, Image, Popup, Embed } from 'semantic-ui-react';
 import axios from 'axios';
 
 export default class Songs extends Component {
@@ -8,6 +8,14 @@ export default class Songs extends Component {
     this.state = {
       songs: []
     }
+  }
+
+  _onMouseMove(e) {
+    let y = event.clientY - event.target.offsetTop
+
+    this.setState({ x: e.clientX, y });
+    console.log(this.state.x, this.state.y)
+
   }
 
   componentDidMount() {
@@ -19,6 +27,7 @@ export default class Songs extends Component {
   }
 
   renderKey(song) {
+
     if(song.inkey) {
       return (
         <a>
@@ -30,38 +39,69 @@ export default class Songs extends Component {
   }
 
   render() {
+    const { x , y } = this.state;
+    const style = {
+      borderRadius: 0,
+      border: '2px solid black',
+      padding: '2em'
+    }
     return (
+
       <Container>
-        <Card.Group itemsPerRow={4}>
-      {this.state.songs.map( (song) => {
-        return (
-          <Card key={song.number}>
-            <Image src={`https://img.youtube.com/vi/${song.videoid}/mqdefault.jpg`} />
-            <Card.Content>
-              <Card.Header>
-                {song.title}
-              </Card.Header>
-              <Card.Meta>
-                <span className='date'>
-                  {new Date(song.date).toDateString()}
-                </span>
-              </Card.Meta>
-              <Card.Description>
-                {song.description}
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              {this.renderKey(song)}
-              <br />
-              <a href={`https://www.youtube.com/watch?v=${song.videoid}`}>
-                <Icon name='video' />
-                Watch
-              </a>
-            </Card.Content>
-          </Card>
-        )
-      })}
-      </Card.Group>
-    </Container>)
+        <Embed
+        id='7Af6b9-yqa8'
+        placeholder={`https://img.youtube.com/vi/7Af6b9-yqa8/mqdefault.jpg`}
+        source='youtube'
+        />
+        <Image.Group size='small' style={{marginTop: '5px', backgroundColor: 'black'}}>
+        {this.state.songs.map(song => {
+          return (
+            <Popup size='tiny' style={style} hoverable inverted trigger={<Image style={{margin: '2px', width: 150, height: 150}} src={`https://img.youtube.com/vi/7Af6b9-yqa8/mqdefault.jpg`} />}>
+              <Popup.Header>{song.title}</Popup.Header>
+              <Popup.Content>
+                <Embed style={{width:'340px', height: '160px'}}
+                  id={song.videoid}
+                  placeholder={`https://img.youtube.com/vi/${song.videoid}/mqdefault.jpg`}
+                  source='youtube'
+                />
+                {this.renderKey(song)}
+              </Popup.Content>
+            </Popup>
+          )
+        })}
+      </Image.Group></Container>
+    )
   }
+    //   <Container>
+    //     <Card.Group itemsPerRow={4}>
+    //   {this.state.songs.map( (song) => {
+    //     return (
+    //       <Card key={song.number}>
+    //         <Image src={`https://img.youtube.com/vi/${song.videoid}/mqdefault.jpg`} />
+    //         <Card.Content>
+    //           <Card.Header>
+    //             {song.title}
+    //           </Card.Header>
+    //           <Card.Meta>
+    //             <span className='date'>
+    //               {new Date(song.date).toDateString()}
+    //             </span>
+    //           </Card.Meta>
+    //           <Card.Description>
+    //             {song.description}
+    //           </Card.Description>
+    //         </Card.Content>
+    //         <Card.Content extra>
+    //           {this.renderKey(song)}
+    //           <br />
+    //           <a href={`https://www.youtube.com/watch?v=${song.videoid}`}>
+    //             <Icon name='video' />
+    //             Watch
+    //           </a>
+    //         </Card.Content>
+    //       </Card>
+    //     )
+    //   })}
+    //   </Card.Group>
+    // </Container>)
 }
