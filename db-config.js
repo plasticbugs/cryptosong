4fb944b40ed01d98da584687c0cfdd58bb6aca27
Scott Moschella <scottmosch@gmail.com>
2018-04-12 23:49:36 -0700

    Add Fawn to db config

--- db-config.js
+++ db-config.js
@@ -1,34 +1,10 @@
-// const Sequelize = require('sequelize');
-// const path = require('path');
-
-// const sequelize = new Sequelize('database', 'username', 'password', {
-//   host: 'localhost',
-//   dialect: 'sqlite',
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000,
-//   },
-//   storage: path.join(__dirname, '/db/database.sqlite'),
-// });
-
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log('Connection has been established successfully.');
-//   })
-//   .catch((err) => {
-//     console.error('Unable to connect to the database:', err);
-//   });
-
-// module.exports = sequelize;
-
-
+const Fawn = require('fawn');
 const mongoose = require('mongoose');
 mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost/cryptosong', {
   promiseLibrary: global.Promise
 });
+
+Fawn.init(mongoose);
 
 module.exports = mongoose.connection;
