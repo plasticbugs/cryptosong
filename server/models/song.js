100ed1d460233791a87d225b0920ae5eb1922595
Scott Moschella <scottmosch@gmail.com>
2018-04-12 23:53:00 -0700

    Only send tag name and image (no subdocs)

--- server/models/song.js
+++ server/models/song.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Models = require('./index.js')
 const TagModel = require('./tag')
+const Fawn = require('fawn');
 
 const tagSchema = new Schema({
   image: String,
@@ -85,7 +86,7 @@ module.exports.getSongByNumberWithAllPossibleTags = (number) => {
         let location = await Models.Location.find();
         let topic = await Models.Topic.find();
         let inkey = await Models.Inkey.find();
-        let tag = await Models.Tag.find();
+        let tag = await Models.Tag.find().select('_id name image');
         resolve({instrument, beard, location, topic, inkey, song, tag});
       })();
 
@@ -110,7 +111,25 @@ const assignIDsToTags = (data) => {
   return songData;
 }
 
+const deleteTagsForSong = (forSongId, task) => {
+  return new Promise((resolve, reject) => {
+    task.update("Song", {number: forSongId}, {tags: []})
+    resolve(task);
+    // Song.findOne({number: forSongId})
+    // .then(song => {
+
+    //   song.tags = [];
+    //   song.save((success => {
+    //     resolve()
+    //   }))
+    // })
+  })
+}
+
+
+
 module.exports.updateSong = (songData) => {
+  const task = new Fawn.Task();
   return new Promise((resolve, reject) => {
     songData = assignIDsToTags(songData);
 
@@ -119,40 +138,99 @@ module.exports.updateSong = (songData) => {
        return instrument._id;
      })
     }
-    TagModel.deleteTagsForSong(Number.parseInt(songData.number))
-    .then(success => {
+    deleteTagsForSong(Number.parseInt(songData.number, 10), task)
+    .then(task => {
       if (songData.tags.length) {
-        TagModel.addOrInsertTags(songData.tags, Number.parseInt(songData.number))
-        .then(success => {
+        TagModel.addOrInsertTags(songData.tags, Number.parseInt(songData.number, 10), task)
+        .then(task => {
           delete songData.tags;
           songData.length = getLength(songData.mins, songData.secs);
-          Song.findOneAndUpdate({_id: songData._id}, songData, (err, results) => {
-            if (err) {
-              console.log(err);
-              reject(err);
-              return;
-            }
-            resolve(results);
+          task.update("Song", {_id: songData._id}, songData);
+          task.run({useMongoose: true})
+          .then(success => {
+            console.log('saved?')
+            Song.findById(songData._id)
+            .then(song => {
+              console.log(song.title)
+              resolve(song);
+            })
           })
+          .catch(err => {
+            
+            console.log("oh snamp:", err)
+          })
+
+          // Song.findOneAndUpdate({_id: songData._id}, songData, (err, results) => {
+          //   if (err) {
+          //     console.log(err);
+          //     reject(err);
+          //     return;
+          //   }
+          //   resolve(results);
+          // })
         })
       } else {
         songData.length = getLength(songData.mins, songData.secs);
-        Song.findOneAndUpdate({_id: songData._id}, songData, (err, results) => {
-          if (err) {
-            console.log(err);
-            reject(err);
-            return;
-          }
-          resolve(results);
-        })
+        task.update("Song", {_id: songData._id}, songData);
+        task.run({useMongoose: true});
+        // Song.findOneAndUpdate({_id: songData._id}, songData, (err, results) => {
+        //   if (err) {
+        //     console.log(err);
+        //     reject(err);
+        //     return;
+        //   }
+        //   resolve(results);
+        // })
       }
 
     })
   })
 }
 
+// module.exports.updateSong = (songData) => {
+//   const task = new Fawn.Task();
+//   return new Promise((resolve, reject) => {
+//     songData = assignIDsToTags(songData);
+
+//     if (songData.instruments.length > 0) {
+//       songData.instruments = songData.instruments.map(instrument => {
+//        return instrument._id;
+//      })
+//     }
+//     deleteTagsForSong(Number.parseInt(songData.number, 10), task)
+//     .then(success => {
+//       if (songData.tags.length) {
+//         TagModel.addOrInsertTags(songData.tags, Number.parseInt(songData.number))
+//         .then(success => {
+//           delete songData.tags;
+//           songData.length = getLength(songData.mins, songData.secs);
+//           Song.findOneAndUpdate({_id: songData._id}, songData, (err, results) => {
+//             if (err) {
+//               console.log(err);
+//               reject(err);
+//               return;
+//             }
+//             resolve(results);
+//           })
+//         })
+//       } else {
+//         songData.length = getLength(songData.mins, songData.secs);
+//         Song.findOneAndUpdate({_id: songData._id}, songData, (err, results) => {
+//           if (err) {
+//             console.log(err);
+//             reject(err);
+//             return;
+//           }
+//           resolve(results);
+//         })
+//       }
+
+//     })
+//   })
+// }
+
 module.exports.insertSong = (newSong) => {
-  console.log('inserting...')
+  const task = new Fawn.Task();
   return new Promise((resolve, reject) => {
     newSong = assignIDsToTags(newSong);
     // newSong.beard = newSong.beard._id;
@@ -164,31 +242,84 @@ module.exports.insertSong = (newSong) => {
     })
 
     newSong.length = getLength(newSong.mins, newSong.secs);
-    let song = new Song(newSong);
-    song.save(err => {
+    const tagArray = Object.assign([], newSong.tags);
+    delete newSong.tags;
+
+    const song = new Song(newSong);
+    song.save((err, savedSong) => {
       if (err) {
         console.log(err);
         reject(err);
+        return;
       }
+
+      if (tagArray.length) {
+        TagModel.addOrInsertTags(tagArray, Number.parseInt(savedSong.number, 10), task)
+        .then(task => {
+          // task.update("Song", {_id: songData._id}, songData);
+          task.run({useMongoose: true})
+          .then(success => {
+            console.log('saved')
+            Song.findById(savedSong._id)
+            .then(song => {
+              console.log(song.title)
+              resolve(song);
+            })
+          })
+          .catch(err => {
+            console.log("oh snap:", err);
+            reject(err);
+          })
+  
+          // Song.findOneAndUpdate({_id: songData._id}, songData, (err, results) => {
+          //   if (err) {
+          //     console.log(err);
+          //     reject(err);
+          //     return;
+          //   }
+          //   resolve(results);
+          // })
+        })
+      } else {
+        songData.length = getLength(songData.mins, songData.secs);
+        task.update("Song", {_id: songData._id}, songData);
+        task.run({useMongoose: true});
+        // Song.findOneAndUpdate({_id: songData._id}, songData, (err, results) => {
+        //   if (err) {
+        //     console.log(err);
+        //     reject(err);
+        //     return;
+        //   }
+        //   resolve(results);
+        // })
+      }
+  
+
+
+
+
+
+
       console.log('it saved');
       resolve('OK');
     })
   })
 }
 
-module.exports.updateTagsOnSongs = (newTagData) => {
+module.exports.updateTagsOnSongs = (newTagData, task) => {
   return new Promise((resolve, reject) => {
-    Song.find({"tags._id": newTagData._id})
-    .then(results => {
-      results.forEach(song => {
-        let tagDoc = song.tags.id(newTagData._id);
-        tagDoc.name = newTagData.name;
-        tagDoc.image = newTagData.image;
-        song.save()
-      })
-      resolve();
+    // Song.find({"tags._id": newTagData._id})
+    // .then(results => {
+      // results.forEach(song => {
+    // let tagDoc = song.tags.id(newTagData._id);
+    task.update("Song", {"tags._id": newTagData._id}, {$set: {"tags.$.name": newTagData.name, "tags.$.image": newTagData.image}})
+    
+        // tagDoc.name = newTagData.name;
+        // tagDoc.image = newTagData.image;
+        // song.save()
+      // })
+      resolve(task);
     })
-  })
 }
 
 module.exports.removeTagsFromSongs = (tagArray) => {
